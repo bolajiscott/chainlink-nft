ea2708ddeebef35e29c262897b8dddcdbfd56285
bolaji ogidan <bolajiscott@yahoo.com>
2022-05-27 22:45:24 +0100

    contracts-added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CurrentVotings from "./pages/CurrentVotings/CurrentVotings";
+
+function App() {
+  return (
+    <div>
+      <CurrentVotings />
+    </div>
+  );
+}
+
+export default App;
